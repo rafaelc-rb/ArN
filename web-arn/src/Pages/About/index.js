dae1a19f7c5994faf3128fc4fe75c6d1a1961ed2
Marcus <marcusbarbosa.aluno@unipampa.edu.br>
2022-06-29 11:21:34 -0300

    Página Sobre finalizada

--- web-arn/src/Pages/About/index.js
+++ web-arn/src/Pages/About/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import image_about from '../../Assets/Img/image_about.png';
 import Header from '../../Components/Header';
 import './styles.css';
+import Footer from '../../Components/Footer';
 
 function About() {
   return (
@@ -12,35 +13,32 @@ function About() {
         <h1 className="title_about">Sobre Nós</h1>
 
         <h3 className="paragraph_about">
-          A Aranet Comunicação Ltda atua no fornecimento de conexão à internet
-          banda larga via fibra óptica e rádio. Licenciada junto a Anatel,
-          atende empresas de pequeno, médio e grande porte, além de usuários
-          residenciais. Com o intuito de sempre sanar as necessidades de
-          conectividade com a rede mundial de computadores, a Aranet conta com
-          profissionais experientes,300px oferecendo a melhor tecnologia
-          disponível e ótima relação custo-benefício.
+        A Aranet Provedor atua no fornecimento de conexão à internet banda larga via fibra óptica e rádio. 
+        Licenciada junto a Anatel, atende apenas usuários residenciais. 
+        Com o intuito de sempre sanar as necessidades de conectividade com a rede mundial de computadores, 
+        a Aranet conta com profissionais experientes, oferecendo a melhor tecnologia disponível e ótima relação custo-benefício.
         </h3>
 
         <img src={image_about} className="image_about" alt="imagem sobre" />
 
         <h3 className="list">
-          <li> Esforços voltados à satisfação do cliente </li>
-          <li> Autonomia com responsabilidade e integridade</li>
-          <li> Compromisso com a qualidade </li>
-          <li> Autonomia com responsabilidade e integridade</li>
-          <li> Autonomia com responsabilidade e integridade</li>
-          <li> Autonomia com responsabilidade e integridade</li>
+        <li>Esforços voltados à satisfação do cliente.</li>
+        <li>Autonomia com responsabilidade e integridade.</li>
+        <li>Compromisso com a qualidade.</li>
+        <li>Inovação e melhoria contínua de produtos e serviços.</li>
+        <li>Valorização Humana.</li>
         </h3>
 
         <h3 className="second_paragraph_about">
-          O resultado da excelência de nossa empresa é mensurado com o seu
-          crescimento. Hoje, atuamos em diversos estados do País: Maranhão /
-          Tocantins / Pará Ser uma empresa reconhecida no setor por sua
-          qualidade, inovação e transparência. Proporcionar aos nossos clientes
-          um serviço de alta qualidade e desempenho. Garantido sempre a
-          satisfação de todos os envolvidos.
+        O resultado da excelência de nossa empresa é mensurado com o seu crescimento. 
+        Hoje, atuamos em diversas cidades do estado: Anápolis / Alexânia / Abadiânia / Ouro Verde. 
+        Ser uma empresa reconhecida no setor por sua qualidade, inovação e transparência.
+        Proporcionar aos nossos clientes um serviço de alta qualidade e desempenho. 
+        Garantido sempre a satisfação de todos os envolvidos.
         </h3>
       </div>
+
+      <Footer />
     </>
   );
 }
