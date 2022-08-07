4ed22d949e2d4f1a5b3bfb4324b317010c5fc2c5
= <tmelo387@gmail.com>
2022-08-07 16:54:33 -0300

    Feat: Listing plans of a city

---
+++ backend-arn/prisma/migrations/20220807185940_migration/migration.sql
@@ -0,0 +1,8 @@
+-- DropIndex
+DROP INDEX "Plan_cityId_key";
+
+-- DropIndex
+DROP INDEX "Plan_technologyId_key";
+
+-- DropIndex
+DROP INDEX "Plan_zoneId_key";
--- backend-arn/src/controller/plan/ListPlansController.ts
+++ backend-arn/src/controller/plan/ListPlansController.ts
@@ -1,18 +1,41 @@
-import { Response } from 'express';
+import { Request, Response } from 'express';
 import { prisma } from '../../service/prisma'
 
 
 class ListPlansController {
-    async execute(res: Response) {
+    async execute(req: Request, res: Response) {
         try {
-            const plans = await prisma.plan.findMany({
-            })
+            const plans = await prisma.plan.findMany
+                (
+                    {
+                        select: {
+                            mb: true,
+                            price: true,
+                            city: {
+                                select:{
+                                    name: true
+                                }
+                            },
+                            zone: {
+                                select:{
+                                    name: true
+                                }
+                            },
+                            technology: {
+                                select:{
+                                    name: true
+                                }
+                            },
+                        }     
+                    }
+                )
             console.log(plans)
             return res.status(200).json(plans)
         } catch (err) {
             return res.status(400).json(err)
         }
     }
+
 }
 
 export { ListPlansController }
